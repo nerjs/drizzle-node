9c6f84e6db8be957617251864af8d0465672eda6
nerjs <karifan.stap@gmail.com>
2019-02-26 23:21:24 +0200

    add window

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const  { Drizzle } = require('drizzle')
-
+const Window = require('window');
 
 const createDrizzleWrapper = customDrizzle => {
     class DrizzleWrapper extends customDrizzle {
         constructor(options, store) {
+            const window = new Window();
+            global.window = window
+            global.document = window.document
+            global.navigator = window.navogator
             super(options, store)
         }
     }
